53ebb0f84f37bbdc62d84a441bd7c26148e3f0f5
Divyanshu Mishra <divyanshumishra3536@gmail.com>
2025-03-10 22:15:44 +0530

    Squashed commit of the following:
    commit d993960153d0d5bbf40c846546b29879d152090b
    Author: Divyanshu Mishra <divyanshumishra3536@gmail.com>
    Date:   Mon Mar 10 22:11:40 2025 +0530
    
        Feature C Addad
    
    commit acd9d3b957eccb7b109974e8f21eb1ae00e71a23
    Author: Divyanshu Mishra <divyanshumishra3536@gmail.com>
    Date:   Mon Mar 10 22:08:25 2025 +0530
    
        Feature B Added
    
    commit 1f3f5a213cba6218fb4658d61c331691b29d3216
    Author: Divyanshu Mishra <divyanshumishra3536@gmail.com>
    Date:   Mon Mar 10 21:55:15 2025 +0530
    
        Feature Addad

--- First.c
+++ First.c
@@ -3,4 +3,9 @@ int main()
 {
     printf("Hello World\n");
     printf("HII Divyanshu");
+    int a,b;
+    printf("Enter Numbers: ");
+    scanf("%d%d",&a,&b);
+    printf("Result=%d",a+b);
+    return 0;
 }
--- Rust/Third.c
+++ Rust/Third.c
@@ -3,4 +3,9 @@ int main()
 {
     printf("BCA student");
     return 0;
+    int a;
+    printf("Enter Number: ");
+    scanf("%d",&a);
+    printf("ASCII Symbol=%c",a);
+    return 0;
 }
