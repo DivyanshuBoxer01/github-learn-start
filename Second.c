8b67b6a93d4f676933586e96b987998a215b5a63
Divyanshu Mishra <divyanshuboxer01@gmail.com>
2025-03-27 23:33:53 +0530

    Loop Feature Added

--- Second.c
+++ Second.c
@@ -6,6 +6,16 @@ int main()
     a=a+b;
     b=a-b;
     a=a-b;
-    printf("Swap Value A=%d And B=%d",a,b);
+    printf("Swap Value A=%d And B=%d",a,b);\
+    p1();
     return 0;
+}
+void p1()
+{
+    int i=1;
+    while(i<=10)
+    {
+        printf("%d",i);
+    }
+
 }
